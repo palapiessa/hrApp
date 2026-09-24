91fb83ae979f60c2058bd6c18434441f2dca2621
Petri Alapiessa <petri.alapiessa@icloud.com>
2026-09-24 20:54:29 +0300

    remove extra elements

--- web/src/pages/EmployeeTablePage.jsx
+++ web/src/pages/EmployeeTablePage.jsx
@@ -1,29 +1,11 @@
 import EmployeesTable from '../components/EmployeesTable';
-import React, { useState } from 'react';
+import React from 'react';
 
 const EmployeeTablePage = () => {
-  const [surname, setSurname] = useState('');
-  const [searchTerm, setSearchTerm] = useState('');
-
-  const handleSearch = () => {
-    setSearchTerm(surname);
-  };
 
   return (
     <div>
-      <div>
-        <label htmlFor="surname">Surname</label>
-        <input
-          id="surname"
-          type="text"
-          value={surname}
-          onChange={(e) => setSurname(e.target.value)}
-        />
-        <button type="button" onClick={handleSearch}>
-          Search
-        </button>
-      </div>
-      <EmployeesTable searchTerm={searchTerm} />
+      <EmployeesTable />
     </div>
   );
 };
